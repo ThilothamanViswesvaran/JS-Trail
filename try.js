338033da73724a29ed4fe5d36bebbf22a81820ef
Thilothaman <edgetiptech@gmail.com>
2025-02-10 17:03:46 +0530

    Add files via upload

---
+++ try.js
@@ -0,0 +1,32 @@
+var homework = true
+
+if(homework)
+{
+    console.log("Great Job!")
+}
+else
+{
+    console.log("Finish your homework before playing")
+}
+
+var cookies = true
+
+if(cookies)
+{
+    console.log("Would you like a cookie?")
+}
+
+else{
+    console.log("Time to bake more cookies.")
+}
+
+var teeth = false
+
+if(teeth)
+{
+    console.log("Your teeth are clean.")
+}
+
+else{
+    console.log("Go brush your teeth now.")
+}
